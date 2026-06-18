9b2889f004de5bc5c52be057d232cd9f9c5bacb7
GVivaLd1 <movigor12@gmail.com>
2026-06-18 20:21:38 +0300

    chore: integrate index.html and style.css into the project environment

--- meteohub/meteohub/settings.py
+++ meteohub/meteohub/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
--- meteohub/meteohub/urls.py
+++ meteohub/meteohub/urls.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
 """
 URL configuration for meteohub project.
 
@@ -15,8 +18,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include('main.urls'))
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
