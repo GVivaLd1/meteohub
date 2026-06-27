5f1f0e1f23356533117d7cda31744dbcbed12962
GVivaLd1 <movigor12@gmail.com>
2026-06-27 15:48:39 +0300

    feat: integrate DB data into comparison grid

--- meteohub/main/views.py
+++ meteohub/main/views.py
@@ -1,5 +1,28 @@
+from datetime import date, timedelta
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import City, WeatherReport
 
 def index(request):
-    return render(request, 'main/index.html')
+    city = City.objects.first()
+    
+    today = date.today()
+    target_dates = [today + timedelta(days=i) for i in range(7)]
+    
+    reports = WeatherReport.objects.filter(city=city, target_date__in=target_dates)
+
+    grouped_data = {}
+    
+    for report in reports:
+        source = report.source
+        
+        if source not in grouped_data:
+            grouped_data[source] = {d: None for d in target_dates}
+            
+        grouped_data[source][report.target_date] = report
+
+    forecast = {
+        'city': city,
+        'dates': target_dates,
+        'grouped_data': grouped_data
+    }
+    return render(request, 'main/index.html', forecast)
