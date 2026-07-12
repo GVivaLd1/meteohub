b66e9357977a7e59976fcf22a16d1b3c47d4cfee
GVivaLd1 <movigor12@gmail.com>
2026-07-12 21:32:17 +0300

    feat: add possibility to switch between general and selected day statistics

--- meteohub/main/static/main/js/script.js
+++ meteohub/main/static/main/js/script.js
@@ -1,5 +1,55 @@
-const sideBarButton = document.getElementById('toggle-sidebar-btn')
+const sideBarButton = document.querySelector("#toggle-sidebar-btn");
+const daysBlock = document.querySelector(".days-of-week");
+const tableStatsBlock = document.querySelector("#table-statistics");
 
-sideBarButton.addEventListener('click', function() {
-    document.getElementById('sidebar').classList.toggle('collapsed');
+const rawDaysDATA = document.querySelector("#days-stats").textContent;
+const daysDATA = JSON.parse(rawDaysDATA);
+
+const generalStats = tableStatsBlock.innerHTML;
+
+sideBarButton.addEventListener("click", () => {
+    document.getElementById("sidebar").classList.toggle("collapsed");
+});
+
+daysBlock.addEventListener("click", (event) => {
+    const clickedEl = event.target.closest("div");
+    if (!clickedEl || !clickedEl.classList.contains("day-name")) return;
+
+    chosenDayDATA = daysDATA[clickedEl.dataset.date];
+    
+    tableStatsBlock.innerHTML = 
+                        `<div id="stats-head">
+                            <h1>Статистика за ${clickedEl.dataset.date}</h1>
+                            <button>X</button>
+                        </div>
+                        <div id="stats-body">
+
+                            <div class="stat-card">
+                                <span class="stat-name">Максимальна температура</span>
+                                <span class="stat-content">${chosenDayDATA.highest_temp[0]}</span>
+                                <span class="stat-source">${chosenDayDATA.highest_temp[1]}</span>
+                            </div>
+
+                            <div class="stat-card">
+                                <span class="stat-name">Мінімальна температура</span>
+                                <span class="stat-content">${chosenDayDATA.lowest_temp[0]}</span>
+                                <span class="stat-source">${chosenDayDATA.lowest_temp[1]}</span>
+                            </div>
+
+                            <div class="stat-card">
+                                <span class="stat-name">Найчастіша погода</span>
+                                <span class="stat-content">${chosenDayDATA.most_frequent_condition}</span>
+                            </div>
+
+                            <div class="stat-card">
+                                <span class="stat-name">Найрідкісніша погода</span>
+                                <span class="stat-content">${chosenDayDATA.least_frequent_condition}</span>
+                            </div>
+                        </div>`
+
+    const exitBtn = tableStatsBlock.querySelector("button");
+
+    exitBtn.addEventListener("click", () => {
+        tableStatsBlock.innerHTML = generalStats;
+    });
 });
